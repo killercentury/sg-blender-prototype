dec15b510edd9c3b2793d38d889e3d939227466c
Kira Cheung <killercentury@gmail.com>
2016-01-07 01:08:34 +1100

    Allow to concatenate dependencies and save as a zip file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
  * Controller of the angularStarterApp
  */
 angular.module('angularStarterApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, $q) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.download = function () {
+      var promise1 = $http.get('/bower_components/foundation/css/foundation.css');
+      var promise2 = $http.get('/bower_components/bootstrap/dist/css/bootstrap.css');
+
+      $q.all([promise1, promise2]).then(function (response) {
+        var css = response[0].data + response[1].data;
+
+        var zip = new JSZip();
+        var cssFolder = zip.folder('css');
+        cssFolder.file('styleguide.css', css);
+        zip.file('config.json', 'myconfig');
+        var content = zip.generate({
+          type: 'blob'
+        });
+        saveAs(content, "styleguide.zip");
+      });
+    };
+
   });
